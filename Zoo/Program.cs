c02f2eda55b0a4b402ac89ed005ebe215ed14cca
John Tse <jtse63@gmail.com>
2023-02-10 23:36:26 -0500

    Loaded classes
    Classes loaded into zoo

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 // See https://aka.ms/new-console-template for more information
 
 
@@ -6,26 +8,86 @@ namespace Zoo
 {
     interface IAnimal
     {
+        //properties
         string Name { get; set; }
         string Description { get; set; }
         int Quantity { get; set; }
+        public void addQuantity(int q);
+        public void removeQuantity(int q);
+        public delegate void AnimalEventHandler(object source, EventArgs args);
     }
-    class Animal: IAnimal
+    class Animal : IAnimal
     {
+        //properties
         public string Name { get; set; } = "";
         public string Description { get; set; } = "";
         public int Quantity { get; set; } = 0;
+
+        //instance methods
+        public void addQuantity(int q)
+        {
+            Quantity += q;
+        }
+        public void removeQuantity(int q)
+        {
+            Quantity -= q;
+            if (Quantity == 0)
+            {
+                Console.WriteLine("There are no " + Name+'s' + " in the zoo.");
+            }
+        }
+    }
+    public class LoadAnimal
+    {
+        //Delegate
+        public delegate void AnimalEventHandler(object source, EventArgs args);
+        //Event
+        public event AnimalEventHandler animaltransport;
+
+        public void waitAnimal(Animal animal)
+        {
+            Console.WriteLine(animal.Name + " is in the process of being transported.");
+            Thread.Sleep(1000);
+
+            animalImport();
+        }
+
+        protected virtual void animalImport()
+        {
+            if (animaltransport != null)
+            {
+                animaltransport(this, null);
+            }
+        }
+    }
+
+    public class animalEntered
+    {
+        public void animalImport(object source, EventArgs eventArgs)
+        {
+            Console.WriteLine("Animal was placed into enclosure.");
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
-        {
-            Animal lion = new Animal();
+        {   
+            //class declarations
+            var lion = new Animal();
+            var animalLoad = new LoadAnimal();
+            var animalEntered = new animalEntered();
+            //property definitions
             lion.Name = "Lion";
             lion.Description = "I am a lion.";
             lion.Quantity = 3;
-            Console.WriteLine(lion.Name +' '+ lion.Description+' '+ lion.Quantity);
+
+            //instance method change
+            lion.addQuantity(5);
+            lion.removeQuantity(8);
+
+            animalLoad.animaltransport += animalEntered.animalImport();
+            animalLoad.waitAnimal(animal);
         }
     }
 }
